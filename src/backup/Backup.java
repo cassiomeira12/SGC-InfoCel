139e0693d278247d9c56811bfef09a90ceb159a1
pedro <pedro.pppp@hotmail.com>
2018-06-06 03:16:53 -0300

    PEDRO - BACKUP

--- src/backup/Backup.java
+++ src/backup/Backup.java
@@ -4,102 +4,60 @@ package backup;
  *
  * @author pedro
  */
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.*;
 
-import java.io.File;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Backup {
 
-    // Constantes da classe
-    private static String VERSION = "4.0.3";
+    private static String url = "jdbc:mysql://neolig.com:3306/";
+    //private static String url = "jdbc:mysql://localhost:3306/";
+    private static String port = "3306";
+    private static String database = "neoli831_teste";
+    private static String user = "neoli831_teste";
+    private static String pass = "teste";
 
-    private static String SEPARATOR = File.separator;
-
-    private static String MYSQL_PATH
-            = "G:" + SEPARATOR
-            + "Arquivos de programas" + SEPARATOR
-            + "MySQL" + SEPARATOR
-            + "MySQL Server 5.0" + SEPARATOR
-            + "bin" + SEPARATOR;
-
-    // Lista dos bancos de dados a serem "backupeados"; se desejar adicionar mais,
-    // basta colocar o nome separado por espaços dos outros nomes
-    private static String DATABASES
-            = "agenda cultos webcheckadmin projectmanager calendario webfinance mysql";
-
-    private List<String> dbList = new ArrayList<String>();
-
-    public Backup() {
-
-        String command = MYSQL_PATH + "mysqldump.exe";
-
-        String[] databases = DATABASES.split(" ");
-
-        for (int i = 0; i < databases.length; i++) {
-            dbList.add(databases[i]);
-        }
-
-        System.out.println("Iniciando backups...\n\n");
-
-        // Contador
-        int i = 1;
-
-        // Tempo
-        long time1, time2, time;
-
-        // Início
-        time1 = System.currentTimeMillis();
-
-        for (String dbName : dbList) {
-
-            ProcessBuilder pb = new ProcessBuilder(
-                    command,
-                    "--user=root",
-                    "--password=trotski123",
-                    dbName,
-                    "--result-file=" + "." + SEPARATOR + "Backup" + SEPARATOR + dbName + ".sql");
-
-            try {
-
-                System.out.println(
-                        "Backup do banco de dados (" + i + "): " + dbName + " ...");
-
-                pb.start();
-
-            } catch (Exception e) {
-
-                e.printStackTrace();
-
+    public static boolean exportar(String path) {
+        String dumpCommand = "mysqldump " + database + " -h " + url + " -u " + user + " -p" + pass;
+        Runtime rt = Runtime.getRuntime();
+        File test = new File(path);
+        PrintStream ps;
+        try {
+            Process child = rt.exec(dumpCommand);
+            ps = new PrintStream(test);
+            InputStream in = child.getInputStream();
+            int ch;
+            while ((ch = in.read()) != -1) {
+                ps.write(ch);
             }
 
-            i++;
-
+            InputStream err = child.getErrorStream();
+            while ((ch = err.read()) != -1) {
+                System.out.write(ch);
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+            return false;
         }
 
-        // Fim
-        time2 = System.currentTimeMillis();
-
-        // Tempo total da operação
-        time = time2 - time1;
-
-        // Avisa do sucesso
-        System.out.println("\nBackups realizados com sucesso.\n\n");
-
-        System.out.println("Tempo total de processamento: " + time + " ms\n");
-
-        System.out.println("Finalizando...");
-
-        try {
-
-            // Paralisa por 2 segundos
-            Thread.sleep(2000);
-
-        } catch (Exception e) {
-        }
-
-        // Termina o aplicativo
-        System.exit(0);
-
+        return true;
     }
 
+    public static boolean importar(String path) {
+        try {
+            String comando = "/usr/bin/mysql";
+            ProcessBuilder pb = new ProcessBuilder(comando, "--user=" + user, "--password=" + pass, "localhost/" + database, "--execute=source " + path);
+            pb.start();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return true;
+    }
 }
--- src/controller/BackupRestauracaoConfiguracoesController.java
+++ src/controller/BackupRestauracaoConfiguracoesController.java
@@ -5,15 +5,26 @@
  */
 package controller;
 
+import backup.Backup;
+import banco.ControleDAO;
+import static controller.LoginController.admLogado;
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.FileChooser;
+import javax.swing.JFileChooser;
+import javax.swing.SwingWorker;
+import util.alerta.Alerta;
 
 /**
  * FXML Controller class
@@ -32,25 +43,119 @@ public class BackupRestauracaoConfiguracoesController implements Initializable {
     private Label ultimoBackupLabel;
     @FXML
     private Label proximoBackupLabel;
+    @FXML
+    private Button btnImportar, btnBackup, btnAlterar;
+
+    @FXML
+    private StackPane stackPane;
+    private ProgressIndicator indicator = new ProgressIndicator();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        stackPane.getChildren().add(indicator);
+        indicator.setVisible(false);
     }
 
     @FXML
     private void realizarBackup(ActionEvent event) {
+        btnAlterar.setDisable(true);
+        btnBackup.setDisable(true);
+        btnImportar.setDisable(true);
+        caminhoBackupText.setDisable(true);
+        indicator.setVisible(true);
+
+        //Metodo executado numa Thread separada
+        SwingWorker<Boolean, Boolean> worker = new SwingWorker<Boolean, Boolean>() {
+            @Override
+            protected Boolean doInBackground() throws Exception {
+                if (Backup.exportar(caminhoBackupText.getText())) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            //Metodo chamado apos terminar a execucao numa Thread separada
+            @Override
+            protected void done() {
+                btnAlterar.setDisable(false);
+                btnBackup.setDisable(false);
+                btnImportar.setDisable(false);
+                caminhoBackupText.setDisable(false);
+                indicator.setVisible(false);
+                super.done(); //To change body of generated methods, choose Tools | Templates.
+
+                try {
+                    if (get()) {
+                        Alerta.info("Nome do arquivo: " + (new File(caminhoBackupText.getText())).getName(), "Backup realizado com sucesso!");
+                    } else {
+                        Alerta.erro("Erro ao realizar backup");
+                    }
+                } catch (Exception e) {
+                    Alerta.erro("Erro ao realizar backup", e.getMessage());
+                }
+            }
+        };
+
+        worker.execute();
     }
 
     @FXML
     private void importar(ActionEvent event) {
+         FileChooser chooser = new FileChooser();
+        chooser.setTitle("Escolha o diretória para backup");
+
+        String path = chooser.showOpenDialog(null).getAbsoluteFile().getAbsolutePath();
+        caminhoBackupText.setText(path);
+        indicator.setVisible(true);
+        
+         //Metodo executado numa Thread separada
+        SwingWorker<Boolean, Boolean> worker = new SwingWorker<Boolean, Boolean>() {
+            @Override
+            protected Boolean doInBackground() throws Exception {
+                if (        Backup.importar(caminhoBackupText.getText())) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            //Metodo chamado apos terminar a execucao numa Thread separada
+            @Override
+            protected void done() {
+                btnAlterar.setDisable(false);
+                btnBackup.setDisable(false);
+                btnImportar.setDisable(false);
+                caminhoBackupText.setDisable(false);
+                indicator.setVisible(false);
+                super.done(); //To change body of generated methods, choose Tools | Templates.
+
+                try {
+                    if (get()) {
+                        Alerta.info("Nome do arquivo: " + (new File(caminhoBackupText.getText())).getName(), "Importado com sucesso!");
+                    } else {
+                        Alerta.erro("Erro ao realizar importação");
+                    }
+                } catch (Exception e) {
+                    Alerta.erro("Erro ao realizar importação", e.getMessage());
+                }
+            }
+        };
+
+        worker.execute();
     }
 
     @FXML
     private void alterar(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Escolha o diretória para backup");
+
+        String path = chooser.showSaveDialog(null).getAbsoluteFile().getAbsolutePath();
+        caminhoBackupText.setText(path);
+
     }
-    
+
 }
